dfcda1173a172ab590677113a6ed6ec9a72b58da
Spencer McCoubrey <spencer.mccoubrey@mail.utoronto.ca>
2019-12-13 01:16:40 -0500

    adding test

--- lambda/server/server.js
+++ lambda/server/server.js
@@ -8,12 +8,10 @@ require('dotenv').config();
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  // res.write('<h1>'+ process.env.MONGODB_URL + process.env.JWT_SECRET + '</h1>');
-  res.status(200);
-  res.json({jwt: process.env.JWT_SECRET, db: process.env.MONGODB_URL, exp: process.env.JWT_EXP});
-  // res.write('<h1>Hello from Express.js!</h1><p>'+ jwt.sign({ sub: 'hello' }, process.env.JWT_SECRET, {
-  //   expiresIn: process.env.JWT_EXP,
-  // }) + '</p>');
+  res.write('<h1>'+ process.env.MONGODB_URL + process.env.JWT_SECRET + process.env.JWT_EXP + '</h1>');
+  res.write('<h1>Hello from Express.js!</h1><p>'+ jwt.sign({ sub: 'hello' }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP,
+  }) + '</p>');
   res.end();
 });
 
