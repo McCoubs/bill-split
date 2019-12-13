7b52645171120d5fab2179570568835be3855eba
Spencer McCoubrey <spencer.mccoubrey@mail.utoronto.ca>
2019-12-13 00:30:39 -0500

    adding serverless functions for auth

---
+++ create-env.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const token = crypto.randomBytes(64).toString('hex');
+fs.writeFileSync('./.env', `JWT_SECRET=${token}\nMONGODB_URL=${process.env.MONGODB_URL}\nJWT_EXP=1800`);
