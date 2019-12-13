4841e02b6e49244140322bbd8a31abf8c2c54ed4
Spencer McCoubrey <spencer.mccoubrey@mail.utoronto.ca>
2019-12-13 00:59:10 -0500

    adding test

--- create-env.js
+++ create-env.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 const token = crypto.randomBytes(64).toString('hex');
+console.log(`JWT_SECRET=${token}\nMONGODB_URL=${process.env.MONGODB_URL}\nJWT_EXP=1800`);
 fs.writeFileSync('./.env', `JWT_SECRET=${token}\nMONGODB_URL=${process.env.MONGODB_URL}\nJWT_EXP=1800`);
