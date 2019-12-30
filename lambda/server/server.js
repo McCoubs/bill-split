febfde37d12bd7c1ffdc414707c8e2e56114866c
Spencer McCoubrey <spencer.mccoubrey@mail.utoronto.ca>
2019-12-30 00:26:33 -0500

    fix

--- lambda/server/server.js
+++ lambda/server/server.js
@@ -2,14 +2,38 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
-const { authRoutes } = require('../utils/googleAuth');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 require('dotenv').config();
 
 const app = express();
 const bodyParser = require('body-parser');
 
 let router = express.Router();
-router = authRoutes(router);
+
+router.post('/google-auth-login', (req, res) => {
+  const idToken = req.body.idToken;
+  if (!!idToken) {
+    client.verifyIdToken({
+      idToken,
+      audience: process.env.GOOGLE_CLIENT_ID
+    }).then((ticket) => {
+      // set cookie for session management
+      res.cookie('googleId', idToken, { httpOnly: true, secure: true /* process.env.NODE_ENV === 'production' */, sameSite: true });
+      // send back payload
+      res.json(ticket.getPayload());
+    }).catch(() => {
+      res.status(401).send('Provided idToken was invalid');
+    });
+  } else {
+    res.status(401).send('No idToken was provided');
+  }
+});
+
+router.post('/google-auth-logout', (req, res) => {
+
+});
+
+// router = authRoutes(router);
 
 app.use(cors({credentials: true, origin: '*'}));
 app.use(bodyParser.json());
